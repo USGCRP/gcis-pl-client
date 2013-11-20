9abaefbfc7eed0f144c8c2132fae657d96ff4caf
Brian Duggan <bduggan@usgcrp.gov>
2013-11-20 16:49:17 -0500

    assign findings

--- eg/assign-keywords.pl
+++ eg/assign-keywords.pl
@@ -6,32 +6,21 @@ use Data::Dumper;
 use v5.14;
 
 my $c = Tuba::Client->new->find_credentials->login;
+my $findings = $c->get('/report/nca3draft/finding');
 
-my $finding = 'more-needed-to-reduce-us-emission';
-
-my $existing = $c->get(qq[/report/nca3draft/chapter/our-changing-climate/finding/$finding]);
-
-#say Dumper($existing->{gcmd_keywords});
-#say Dumper($existing->{keywords});
-
-my $this =
-           {
-             'id' => '5286',
-             'category' => 'EARTH SCIENCE',
-             'topic' => 'HUMAN DIMENSIONS',
-             'term' => 'ENVIRONMENTAL IMPACTS',
-             'level1' => 'FOSSIL FUEL BURNING',
-             'level2' => undef,
-             'level3' => undef,
-           };
-my $i = 1;
-for my $this (@{ $existing->{keywords} }) { 
-    $this->{_delete_extra} = 1 if $i==1;
-    say $i++;
-    my $got = $c->post("/report/nca3draft/chapter/our-changing-climate/finding/keywords/$finding",
-        $this
-    );
-    say Dumper($this);
-    say Dumper($got);
+for my $f (@$findings) {
+    my $finding_identifer = $f->{identifier};
+    say $finding_identifer;
+    my $existing = $c->get($f->{uri});
+    my $i = 1;
+    my $post = $f->{uri};
+    $post =~ s[finding][finding/keywords];
+    for my $this (@{ $existing->{keywords} }) { 
+        $this->{_delete_extra} = 1 if $i==1;
+        say $i++;
+        my $got = $c->post($post, #"/report/nca3draft/chapter/our-changing-climate/finding/keywords/$finding",
+            $this
+        );
+    }
 }
 
