dc65c6089bb576822edc13ef4be461cdf4fac67e
Brian Duggan <bduggan@usgcrp.gov>
2014-05-21 15:06:21 -0600

    updated example

--- eg/create_report.pl
+++ eg/create_report.pl
@@ -6,6 +6,7 @@ use Gcis::Client;
 
 my $c = Gcis::Client->connect(url => $ARGV[0]);
 
+goto CHAPTER;
 $c->post(
   '/report',
   {
@@ -17,5 +18,20 @@ $c->post(
     publication_year => '2000',
     url              => "http://example.com/report.pdf",
   }
-);
+) or die $c->error;
+
+CHAPTER:
+say "adding";
+# Add a chapter
+$c->post(
+    "/report/my-new-report/chapter",
+    {
+        report_identifier => "my-report",
+        identifier        => "my-chapter-identifier",
+        title             => "Some Title",
+        number            => 12,
+        sort_key          => 100,
+        # optional DOI
+    }
+) or die $c->error;
 
